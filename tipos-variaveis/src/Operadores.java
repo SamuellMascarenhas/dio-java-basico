0f5b17da419f761e98e622718945929abd688285
Samuel Mascarenhas <samuelmascarenhascontato@gmail.com>
2024-05-10 15:22:19 -0300

    update

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+    //     String nomeCompleto = "LINGUAGEM" + "JAVA";
+    //     System.out.println(nomeCompleto);
+
+
+    // boolean variavel = true;
+    // System.out.println(!variavel);
+    // //    variavel = !variavel;
+    // //    System.out.println(variavel);
+
+
+    //  operador ternário:
+    // int a, b;
+    //  a = 5;
+    //   b = 6;
+    //   String resultado = a==b ? "verdadeiro" : "falso";
+    //   System.out.println(resultado);
+
+// operadores relacionais
+// int numero1 = 1;
+// int numero2 = 2;
+
+// boolean simNao = numero1 == numero2;
+// System.out.println ("numeroUm é igual a numeroDois?" + simNao);
+
+// operadores lógicos
+
+     }
+  
+ 
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+// Variável, é uma identificação de um espaço em memória, utilizado pelo nosso programa.
+// composta por tipo de dados + identificação + valor atribuído.
+// Tipos de dados
+// int, double, float, char, boolean, String
+// 
+
+// public class TiposVariaveis {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+
